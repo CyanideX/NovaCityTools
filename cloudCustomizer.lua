148a22c4420eb96f0504b7047ac2224becdb4116
CyanideX <3106700+CyanideX@users.noreply.github.com>
2024-10-15 21:36:30 -0700

    Cloud customizer styling

--- cloudCustomizer.lua
+++ cloudCustomizer.lua
@@ -56,37 +56,64 @@ function M.ApplySettings()
     GameOptions.SetFloat("Editor/VolumetricClouds", "DetailNoiseScale", settings.Current.detail)
 end
 
-
 function M.DrawCloudCustomizer(cetOpen)
     if not cetOpen then
         return
     end
-    
-    if ImGui.Begin("Cloud Customizer", ImGuiWindowFlags.NoScrollbar) then
-        ImGui.PushItemWidth(220)
-        settings.Current.coverage, changed = ImGui.SliderFloat("Coverage Scale", settings.Current.coverage, 1.0, 10.0)
-        if changed then
-            GameOptions.SetFloat("Editor/VolumetricClouds", "CoverageScale", settings.Current.coverage)
-            M.SaveSettings()
+
+    ImGui.SetNextWindowSizeConstraints(292, 300, width / 100 * 47, 300)
+    if ImGui.Begin("Cloud Customizer", ImGuiWindowFlags.NoScrollbar + ImGuiWindowFlags.AlwaysUseWindowPadding) then
+        ImGui.PushStyleColor(ImGuiCol.ChildBg, ImGui.GetColorU32(0.65, 0.7, 1, 0.045)) -- Set your desired color here
+
+        -- Set inner padding for the child window
+        ImGui.PushStyleVar(ImGuiStyleVar.ChildRounding, 5.0)
+        ImGui.PushStyleVar(ImGuiStyleVar.FramePadding, 10, 6)
+        ImGui.SetWindowFontScale(0.85)
+
+        if ImGui.BeginChild("CloudCustomizer", ImGui.GetContentRegionAvail()) then
+            ImGui.PushItemWidth(-1) -- Makes the item width span the entire child window
+
+            ImGui.Text("Coverage Scale")
+            settings.Current.coverage, changed = ImGui.SliderFloat("##Coverage Scale", settings.Current.coverage, 1.0, 10.0)
+            if changed then
+                GameOptions.SetFloat("Editor/VolumetricClouds", "CoverageScale", settings.Current.coverage)
+                M.SaveSettings()
+            end
+
+            ImGui.Text("Noise Scale")
+            settings.Current.noise, changed = ImGui.SliderFloat("##Noise Scale", settings.Current.noise, 0.4, 10.0)
+            if changed then
+                GameOptions.SetFloat("Editor/VolumetricClouds", "NoiseScale", settings.Current.noise)
+                M.SaveSettings()
+            end
+
+            ImGui.Text("Detail Scale")
+            settings.Current.detail, changed = ImGui.SliderFloat("##Detail Scale", settings.Current.detail, 0.4, 10.0)
+            if changed then
+                GameOptions.SetFloat("Editor/VolumetricClouds", "DetailNoiseScale", settings.Current.detail)
+                M.SaveSettings()
+            end
+
+            ImGui.Dummy(0, 10) -- Add some space between sliders and buttons
+
+            local buttonWidth = (ImGui.GetContentRegionAvail() - ImGui.GetStyle().ItemSpacing.x) / 2
+
+            if ImGui.Button("Randomize", buttonWidth, 0) then
+                M.RandomizeClouds()
+            end
+            ImGui.SameLine()
+            if ImGui.Button("Default", buttonWidth, 0) then
+                M.DefaultClouds()
+            end
+
+            ImGui.PopItemWidth()
+            ImGui.EndChild()
         end
-        settings.Current.noise, changed = ImGui.SliderFloat("Noise Scale", settings.Current.noise, 0.4, 10.0)
-        if changed then
-            GameOptions.SetFloat("Editor/VolumetricClouds", "NoiseScale", settings.Current.noise)
-            M.SaveSettings()
-        end
-        settings.Current.detail, changed = ImGui.SliderFloat("Detail Scale", settings.Current.detail, 0.4, 10.0)
-        if changed then
-            GameOptions.SetFloat("Editor/VolumetricClouds", "DetailNoiseScale", settings.Current.detail)
-            M.SaveSettings()
-        end
-        if ImGui.Button("Randomize") then
-            M.RandomizeClouds()
-        end
-        ImGui.SameLine()
-        if ImGui.Button("Default") then
-            M.DefaultClouds()
-        end
-        ImGui.End()
+
+        ImGui.SetWindowFontScale(1)
+
+        ImGui.PopStyleVar(2)
+        ImGui.PopStyleColor()
     end
 end
 
